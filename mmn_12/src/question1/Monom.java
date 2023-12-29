d2533c64e30c6d844261111deccb093fa00e6ff7
shanike <56911506+shanike@users.noreply.github.com>
2023-12-29 11:34:02 +0200

    style(mmn_12, q1): eclipse fmt

--- mmn_12/src/question1/Monom.java
+++ mmn_12/src/question1/Monom.java
@@ -1,36 +1,36 @@
 package question1;
 
 public class Monom implements Comparable<Object> {
-    double coefficient;
-    int exponent;
+	double coefficient;
+	int exponent;
 
-    public Monom(double coefficient, int exponent) {
-        this.coefficient = coefficient;
-        this.exponent = exponent;
-    }
+	public Monom(double coefficient, int exponent) {
+		this.coefficient = coefficient;
+		this.exponent = exponent;
+	}
 
-    public void addToCoefficient(double valueToAdd) {
-        this.coefficient += valueToAdd;
-    }
+	public void addToCoefficient(double valueToAdd) {
+		this.coefficient += valueToAdd;
+	}
 
-    public void subtractFromCoefficient(double valueToSubtract) {
-        this.coefficient -= valueToSubtract;
-    }
+	public void subtractFromCoefficient(double valueToSubtract) {
+		this.coefficient -= valueToSubtract;
+	}
 
-    /**
-     * A function for {@code Collections} to call when {@code sort}ing an
-     * {@code ArrayList} of {@code Monom}s
-     */
-    public int compareTo(Object o) {
-        if (!(o instanceof Monom)) {
-            // Shouldn't happen. But in case: send it to the end of the list.
-            return -1;
-        }
-        Monom otherMonom = (Monom) o;
-        return otherMonom.exponent - this.exponent;
-    }
+	/**
+	 * A function for {@code Collections} to call when {@code sort}ing an
+	 * {@code ArrayList} of {@code Monom}s
+	 */
+	public int compareTo(Object o) {
+		if (!(o instanceof Monom)) {
+			// Shouldn't happen. But in case: send it to the end of the list.
+			return -1;
+		}
+		Monom otherMonom = (Monom) o;
+		return otherMonom.exponent - this.exponent;
+	}
 
-    public String toString() {
-        return coefficient + "x^" + exponent;
-    }
+	public String toString() {
+		return coefficient + "x^" + exponent;
+	}
 }
--- mmn_12/src/question1/Polynom.java
+++ mmn_12/src/question1/Polynom.java
@@ -5,212 +5,209 @@ import java.util.Collections;
 
 public class Polynom {
 
-    private ArrayList<Monom> monoms;
+	private ArrayList<Monom> monoms;
 
-    private static final String BAD_REQUEST_MESSAGE = "BAD_REQUEST";
+	private static final String BAD_REQUEST_MESSAGE = "BAD_REQUEST";
 
-    /**
-     * Creates a new Polynom instance.
-     * 
-     * If exponents have duplicate values: will add the coefficients to a single
-     * exponent
-     * 
-     * @param coefficients
-     * @param exponents
-     * @throws Exception if coefficient or exponents array are invalid:
-     *                   1) If the arrays are not the same length.
-     *                   2) If there's a non-natural integer in {@code exponents}
-     */
-    public Polynom(double[] coefficients, int[] exponents) throws Exception {
-        if (coefficients.length != exponents.length) {
-            throw new Exception(BAD_REQUEST_MESSAGE);
-        }
+	/**
+	 * Creates a new Polynom instance.
+	 * 
+	 * If exponents have duplicate values: will add the coefficients to a single
+	 * exponent
+	 * 
+	 * @param coefficients
+	 * @param exponents
+	 * @throws Exception if coefficient or exponents array are invalid: 1) If the
+	 *                   arrays are not the same length. 2) If there's a non-natural
+	 *                   integer in {@code exponents}
+	 */
+	public Polynom(double[] coefficients, int[] exponents) throws Exception {
+		if (coefficients.length != exponents.length) {
+			throw new Exception(BAD_REQUEST_MESSAGE);
+		}
 
-        for (int exponent : exponents) {
-            if (exponent < 0) {
-                throw new Exception(BAD_REQUEST_MESSAGE);
-            }
-        }
+		for (int exponent : exponents) {
+			if (exponent < 0) {
+				throw new Exception(BAD_REQUEST_MESSAGE);
+			}
+		}
 
-        Polynom polynom = new Polynom();
-        int polynomLength = coefficients.length;
-        for (int i = 0; i < polynomLength; i++) {
-            double coefficient = coefficients[i];
-            int exponent = exponents[i];
-            if (coefficient == 0) {
-                // If coefficient is 0 then the exponent's value doesn't matter
-                exponent = 0;
-            }
-            Monom monom = new Monom(coefficient, exponent);
-            polynom.addMonomToPolynom(monom);
-        }
-        Collections.sort(polynom.monoms);
-        this.monoms = polynom.monoms;
-    }
+		Polynom polynom = new Polynom();
+		int polynomLength = coefficients.length;
+		for (int i = 0; i < polynomLength; i++) {
+			double coefficient = coefficients[i];
+			int exponent = exponents[i];
+			if (coefficient == 0) {
+				// If coefficient is 0 then the exponent's value doesn't matter
+				exponent = 0;
+			}
+			Monom monom = new Monom(coefficient, exponent);
+			polynom.addMonomToPolynom(monom);
+		}
+		Collections.sort(polynom.monoms);
+		this.monoms = polynom.monoms;
+	}
 
-    /**
-     * Create an empty Polynom.
-     */
-    private Polynom() {
-        this.monoms = new ArrayList<Monom>();
-    }
+	/**
+	 * Create an empty Polynom.
+	 */
+	private Polynom() {
+		this.monoms = new ArrayList<Monom>();
+	}
 
-    /**
-     * @param otherPolynom to add to {@code this} polynom
-     * @return a new polynom representing the result of adding {@code otherPolynom}
-     *         to {@code this} polynom
-     * @throws Exception if the generated polynom is invalid.
-     * @note - another way is to build arrays of exponents and coefficients. But
-     *       that seems unstable because the two arrays need to be strongly related
-     */
-    public Polynom plus(Polynom otherPolynom) throws Exception {
+	/**
+	 * @param otherPolynom to add to {@code this} polynom
+	 * @return a new polynom representing the result of adding {@code otherPolynom}
+	 *         to {@code this} polynom
+	 * @throws Exception if the generated polynom is invalid.
+	 * @note - another way is to build arrays of exponents and coefficients. But
+	 *       that seems unstable because the two arrays need to be strongly related
+	 */
+	public Polynom plus(Polynom otherPolynom) throws Exception {
 
-        Polynom summedPolynom = new Polynom(new double[] {}, new int[] {});
+		Polynom summedPolynom = new Polynom(new double[] {}, new int[] {});
 
-        int maxSize = Math.max(this.monoms.size(), otherPolynom.monoms.size());
+		int maxSize = Math.max(this.monoms.size(), otherPolynom.monoms.size());
 
-        for (int i = 0; i < maxSize; i++) {
-            if (i < this.monoms.size()) {
-                summedPolynom.addMonomToPolynom(this.monoms.get(i));
-            }
-            if (i < otherPolynom.monoms.size()) {
-                summedPolynom.addMonomToPolynom(otherPolynom.monoms.get(i));
-            }
-        }
-        Collections.sort(summedPolynom.monoms);
-        return summedPolynom;
-    }
+		for (int i = 0; i < maxSize; i++) {
+			if (i < this.monoms.size()) {
+				summedPolynom.addMonomToPolynom(this.monoms.get(i));
+			}
+			if (i < otherPolynom.monoms.size()) {
+				summedPolynom.addMonomToPolynom(otherPolynom.monoms.get(i));
+			}
+		}
+		Collections.sort(summedPolynom.monoms);
+		return summedPolynom;
+	}
 
-    /**
-     * @param otherPolynom to subtract from {@code this} polynom
-     * @return a new polynom representing the result of subtracting
-     *         {@code otherPolynom} from {@code this} polynom
-     * @throws Exception if the generated polynom is invalid.
-     */
-    public Polynom minus(Polynom otherPolynom) throws Exception {
-        Polynom oppositeOtherPolynom = new Polynom();
-        for (Monom monom : otherPolynom.monoms) {
-            Monom negativeMonom = new Monom(-1 * monom.coefficient, monom.exponent);
-            oppositeOtherPolynom.addMonomToPolynom(negativeMonom);
-        }
-        return this.plus(oppositeOtherPolynom);
-    }
+	/**
+	 * @param otherPolynom to subtract from {@code this} polynom
+	 * @return a new polynom representing the result of subtracting
+	 *         {@code otherPolynom} from {@code this} polynom
+	 * @throws Exception if the generated polynom is invalid.
+	 */
+	public Polynom minus(Polynom otherPolynom) throws Exception {
+		Polynom oppositeOtherPolynom = new Polynom();
+		for (Monom monom : otherPolynom.monoms) {
+			Monom negativeMonom = new Monom(-1 * monom.coefficient, monom.exponent);
+			oppositeOtherPolynom.addMonomToPolynom(negativeMonom);
+		}
+		return this.plus(oppositeOtherPolynom);
+	}
 
-    /**
-     * @return a new polynom representing the derivative of {@code this} Polynom
-     */
-    public Polynom derive() {
-        Polynom derivedPolynom = new Polynom();
-        for (Monom monom : this.monoms) {
-            if (monom.exponent <= 0) {
-                // If exponent is 0: the derived polynom will not have this coefficient
-                continue;
-            }
-            Monom derivedMonom = new Monom(monom.exponent * monom.coefficient, monom.exponent - 1);
-            derivedPolynom.addMonomToPolynom(derivedMonom);
-        }
-        // * If {@code this.monoms} is ordered by exponent, then also derivedPolynom is.
-        return derivedPolynom;
-    }
+	/**
+	 * @return a new polynom representing the derivative of {@code this} Polynom
+	 */
+	public Polynom derive() {
+		Polynom derivedPolynom = new Polynom();
+		for (Monom monom : this.monoms) {
+			if (monom.exponent <= 0) {
+				// If exponent is 0: the derived polynom will not have this coefficient
+				continue;
+			}
+			Monom derivedMonom = new Monom(monom.exponent * monom.coefficient, monom.exponent - 1);
+			derivedPolynom.addMonomToPolynom(derivedMonom);
+		}
+		// * If {@code this.monoms} is ordered by exponent, then also derivedPolynom is.
+		return derivedPolynom;
+	}
 
-    /**
-     * @return whether or not the monoms of the other {@code obj}
-     *         ({@code instanceof Polynom}) are equal to this polynom's monoms
-     */
-    public boolean equals(Object obj) {
-        if (!(obj instanceof Polynom)) {
-            return false;
-        }
+	/**
+	 * @return whether or not the monoms of the other {@code obj}
+	 *         ({@code instanceof Polynom}) are equal to this polynom's monoms
+	 */
+	public boolean equals(Object obj) {
+		if (!(obj instanceof Polynom)) {
+			return false;
+		}
 
-        Polynom otherPolynom = (Polynom) obj;
+		Polynom otherPolynom = (Polynom) obj;
 
-        if (otherPolynom.monoms.size() != this.monoms.size()) {
-            return false;
-        }
-        for (int i = 0; i < this.monoms.size(); i++) {
-            if (this.monoms.get(i).coefficient != otherPolynom.monoms.get(i).coefficient) {
-                return false;
-            }
-            if (this.monoms.get(i).exponent != otherPolynom.monoms.get(i).exponent) {
-                return false;
-            }
-        }
-        return true;
-    }
+		if (otherPolynom.monoms.size() != this.monoms.size()) {
+			return false;
+		}
+		for (int i = 0; i < this.monoms.size(); i++) {
+			if (this.monoms.get(i).coefficient != otherPolynom.monoms.get(i).coefficient) {
+				return false;
+			}
+			if (this.monoms.get(i).exponent != otherPolynom.monoms.get(i).exponent) {
+				return false;
+			}
+		}
+		return true;
+	}
 
-    public String toString() {
-        if (monoms.size() <= 0) {
-            return "Empty polynom";
-        }
+	public String toString() {
+		if (monoms.size() <= 0) {
+			return "Empty polynom";
+		}
 
-        String finalStr = "";
-        for (int i = 0; i < monoms.size(); i++) {
-            Monom monom = monoms.get(i);
+		String finalStr = "";
+		for (int i = 0; i < monoms.size(); i++) {
+			Monom monom = monoms.get(i);
 
-            if (i != 0 && monom.coefficient >= 0) {
-                /*
-                 * The coefficient is a positive number in the middle of the polynom:
-                 * render a preceding "+".
-                 */
-                finalStr += "+";
-            }
+			if (i != 0 && monom.coefficient >= 0) {
+				/*
+				 * The coefficient is a positive number in the middle of the polynom: render a
+				 * preceding "+".
+				 */
+				finalStr += "+";
+			}
 
-            if (Math.abs(monom.coefficient) != 1 || monom.exponent == 0) {
-                /*
-                 * The coefficient is **not** 1 and **not** -1,
-                 * OR it **is** 1 or -1 **but** the exponent is 0:
-                 * render the coefficient.
-                 */
-                finalStr += monom.coefficient;
-            } else if (monom.coefficient == -1) {
-                /**
-                 * The coefficient is -1, (and the exponent is **not** 0) and even though it's
-                 * negative:
-                 * render the "-" because we don't render the coefficient itself when it's -1
-                 */
-                finalStr += "-";
-            }
+			if (Math.abs(monom.coefficient) != 1 || monom.exponent == 0) {
+				/*
+				 * The coefficient is **not** 1 and **not** -1, OR it **is** 1 or -1 **but** the
+				 * exponent is 0: render the coefficient.
+				 */
+				finalStr += monom.coefficient;
+			} else if (monom.coefficient == -1) {
+				/**
+				 * The coefficient is -1, (and the exponent is **not** 0) and even though it's
+				 * negative: render the "-" because we don't render the coefficient itself when
+				 * it's -1
+				 */
+				finalStr += "-";
+			}
 
-            // * SUM: we set the coefficient
+			// * SUM: we set the coefficient
 
-            if (monom.exponent != 0 && monom.coefficient != 0) {
-                /*
-                 * The exponent is **not** 0:
-                 * render the "x" (/and the exponent/).
-                 */
-                finalStr += "x";
+			if (monom.exponent != 0 && monom.coefficient != 0) {
+				/*
+				 * The exponent is **not** 0: render the "x" (/and the exponent/).
+				 */
+				finalStr += "x";
 
-                if (monom.exponent != 1) {
-                    /*
-                     * If the exponent is **not** 1:
-                     * render the exponent.
-                     */
-                    finalStr += "^" + monom.exponent;
-                }
-            }
+				if (monom.exponent != 1) {
+					/*
+					 * If the exponent is **not** 1: render the exponent.
+					 */
+					finalStr += "^" + monom.exponent;
+				}
+			}
 
-        }
-        return finalStr;
-    }
+		}
+		return finalStr;
+	}
 
-    /**
-     * A private method for adding a new Monom to {@code this} polynom
-     */
-    private void addMonomToPolynom(Monom monomToAdd) {
-        boolean doesExponentExists = false;
-        for (Monom monom : this.monoms) {
-            if (monom.exponent == monomToAdd.exponent) {
-                // Exponent exists in this polynom
-                doesExponentExists = true;
-                monom.addToCoefficient(monomToAdd.coefficient);
-                break;
-            }
-        }
-        if (!doesExponentExists) {
-            // This polynom doesn't have this exponent yet
-            Monom copiedMonom = new Monom(monomToAdd.coefficient, monomToAdd.exponent);
-            this.monoms.add(copiedMonom);
-        }
-    }
+	/**
+	 * A private method for adding a new Monom to {@code this} polynom
+	 */
+	private void addMonomToPolynom(Monom monomToAdd) {
+		boolean doesExponentExists = false;
+		for (Monom monom : this.monoms) {
+			if (monom.exponent == monomToAdd.exponent) {
+				// Exponent exists in this polynom
+				doesExponentExists = true;
+				monom.addToCoefficient(monomToAdd.coefficient);
+				break;
+			}
+		}
+		if (!doesExponentExists) {
+			// This polynom doesn't have this exponent yet
+			Monom copiedMonom = new Monom(monomToAdd.coefficient, monomToAdd.exponent);
+			this.monoms.add(copiedMonom);
+		}
+	}
 
 }
