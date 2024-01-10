9b02f03705f09bba3c29cd76c2360feadda2aec8
shanike <56911506+shanike@users.noreply.github.com>
2024-01-10 11:22:13 +0200

    (mmn_13, q1)

--- mmn_13/src/question1/MultiChoiceTestController.java
+++ mmn_13/src/question1/MultiChoiceTestController.java
@@ -22,13 +22,17 @@ public class MultiChoiceTestController {
 
 	private ArrayList<Question> questions = new ArrayList<Question>();
 
-	static private int INITIAL_QUESTION_INDEX = 0;
+	static private final int INITIAL_QUESTION_INDEX = 0;
 	private int currQuestionIndex = INITIAL_QUESTION_INDEX;
 
 	private ArrayList<RadioButton> answerButtons = new ArrayList<RadioButton>();
 
-	static private int INITIAL_SCORE = 0;
+	private ArrayList<Label> resultsLabels = new ArrayList<Label>();
+
+	static private final int INITIAL_SCORE = 0;
 	private int score = INITIAL_SCORE;
+
+	private final String NEXT_BUTTON_TEXT = "NEXT";
 
 	@FXML
 	private GridPane appContainer;
@@ -43,18 +47,19 @@ public class MultiChoiceTestController {
 	private GridPane answersGrid;
 
 	public void initialize() {
+		answersGrid.setPrefWidth(appContainer.getPrefWidth());
 		setQuestions();
+		initializeTest();
+	}
+
+	private void initializeTest() {
+		currQuestionIndex = INITIAL_QUESTION_INDEX;
+		score = INITIAL_SCORE;
 		initializeAnswerNodes();
+		initializeNextButton();
 		showCurrentQuestion();
 		answersGrid.setAlignment(Pos.CENTER);
 		answersGrid.setPadding(new Insets(10, 50, 10, 50));
-	}
-
-	@FXML
-	void handleNextButtonClick(ActionEvent event) {
-		resetSelection();
-		incCurrentQuestionIndex();
-		showCurrentQuestion();
 	}
 
 	/**
@@ -62,36 +67,70 @@ public class MultiChoiceTestController {
 	 */
 	private void setQuestions() {
 		questions.add(new Question("באיזו שנה לבריאת העולם נולד אברהם אבינו?", "1948", "1000", "1", "1722"));
-		questions.add(new Question("Question! the answer is 2?", "2", "not 2", "another not 2", "3"));
+		questions.add(new Question("A question!? The answer is 2", "2", "not 2", "another not 2", "3"));
 		questions.add(
 				new Question("What is the capital city of Australia?", "Canberra", "Sydney", "Brisbane", "Melbourne"));
-//		questions.add(new Question("In which year did the Titanic sink?", "1912", "1905", "1920", "1920"));
-//		questions.add(new Question("Who wrote the play \"Romeo and Juliet\"?", "William Shakespeare", "Charles Dickens",
-//				"Jane Austen", "Mark Twain"));
-//		questions.add(new Question("What is the chemical symbol for gold?", "Au", "Cu", "Fe", "Ag"));
-//		questions.add(new Question("Which planet is known as the Red Planet?", "Mars", "Venus", "Jupiter", "Saturn"));
+		questions.add(new Question("In which year did the Titanic sink?", "1912", "1905", "1920", "1920"));
+		questions.add(new Question("Who wrote the play \"Romeo and Juliet\"?", "William Shakespeare", "Charles Dickens",
+				"Jane Austen", "Mark Twain"));
+		questions.add(new Question("What is the chemical symbol for gold?", "Au", "Cu", "Fe", "Ag"));
+		questions.add(new Question("Which planet is known as the Red Planet?", "Mars", "Venus", "Jupiter", "Saturn"));
 	}
 
 	/**
 	 * Render empty answer nodes to the screen.
 	 */
 	private void initializeAnswerNodes() {
-		// Render empty answers to screen
+		removeResultLabels();
 		for (int i = 0; i < Question.getNumberOfAnswers(); i++) {
 			RadioButton btn = new RadioButton();
-//	        btn.setAlignment(Pos.CENTER);
+			/*
+			 * For UX (User Experience): Set each button to a **third** of the grid, so a
+			 * click close-enough to the button will also trigger the button's onClick.
+			 * 
+			 * - The literal number here is a 3 for dividing by 3 (i.e a third). If extracting
+			 * it to a final int, it would be named "THREE".
+			 */
+			btn.setPrefWidth(answersGrid.getPrefWidth() / 3);
 			answerButtons.add(btn);
 			answersGrid.add(btn, 0, i);
 		}
 	}
 
+	private void removeAnswerNodes() {
+		answersGrid.getChildren().removeAll(answerButtons);
+		answerButtons.removeAll(answerButtons);
+	}
+
+	private void removeResultLabels() {
+		answersGrid.getChildren().removeAll(resultsLabels);
+		resultsLabels.removeAll(resultsLabels);
+	}
+
+	/**
+	 * Set next button properties
+	 */
+	private void initializeNextButton() {
+		nextButton.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				handleNextButtonClick();
+			}
+		});
+		nextButton.setText(NEXT_BUTTON_TEXT);
+	}
+
 	/**
 	 * Set all buttons to {@code isDisabled}
 	 */
-	private void setButtonsDisabled(boolean isDisabled) {
+	private void setAnswerButtonsDisabled(boolean isDisabled) {
 		for (RadioButton btn : answerButtons) {
 			btn.setDisable(isDisabled);
 		}
+	}
+
+	private void setNextButtonDisaled(boolean isDisabled) {
+		nextButton.setDisable(isDisabled);
 	}
 
 	/**
@@ -114,16 +153,32 @@ public class MultiChoiceTestController {
 			return;
 		}
 
-		Question currentQ = questions.get(currQuestionIndex);
-
 		// Set question label
+		setCurrentQuestionLabel();
+
+		// Set answer buttons
+		setCurrentQuestionAnswers();
+
+		// Enable answer buttons
+		setAnswerButtonsDisabled(false);
+
+		// Disable next button
+		setNextButtonDisaled(true);
+
+		// Randomize answers order
+		Collections.shuffle(answerButtons);
+	}
+
+	private void setCurrentQuestionLabel() {
+		Question currentQ = questions.get(currQuestionIndex);
 		questionLabel.setText(currentQ.getQuestion());
 
-		ArrayList<Integer> shuffledAnswersIndexes = getShuffledAnswerIndexes();
-		for (Integer si : shuffledAnswersIndexes) {
-			System.out.println("shuffled index: " + si);
+	}
 
-		}
+	private void setCurrentQuestionAnswers() {
+		Question currentQ = questions.get(currQuestionIndex);
+		ArrayList<Integer> shuffledAnswersIndexes = getShuffledAnswerIndexes();
+
 		int correctAnswerIndex = 0;
 		int incorrectAnswersFirstIndex = correctAnswerIndex + 1;
 		int incorrectAnswersLastIndex = shuffledAnswersIndexes.size() - 1;
@@ -135,20 +190,14 @@ public class MultiChoiceTestController {
 			RadioButton incorrectButton = answerButtons.get(incorrectButtonShuffledIndex);
 			String incorrectAnswer = currentQ.getIncorrectAnswers().get(incorrectAnswerCounter);
 			incorrectButton.setText(incorrectAnswer);
-			incorrectButton.setOnAction(getAnswerClickHandler(false));
+			incorrectButton.setOnAction(createAnswerClickHandler(false));
 			incorrectAnswerCounter++;
 		}
 
 		// Set last button to be correct-answer-button
 		RadioButton correctButton = answerButtons.get(shuffledAnswersIndexes.get(correctAnswerIndex));
 		correctButton.setText(currentQ.getCorrectAnswer());
-		correctButton.setOnAction(getAnswerClickHandler(true));
-
-		// Enable buttons
-		setButtonsDisabled(false);
-
-		// Randomize order
-		Collections.shuffle(answerButtons);
+		correctButton.setOnAction(createAnswerClickHandler(true));
 	}
 
 	/**
@@ -169,7 +218,8 @@ public class MultiChoiceTestController {
 	 * @param isCorrect whether the selected answer is the correct answer
 	 */
 	private void handleAnswerClick(boolean isCorrect) {
-		setButtonsDisabled(true);
+		setAnswerButtonsDisabled(true);
+		setNextButtonDisaled(false);
 		showAnswerResponseAlert(isCorrect);
 		if (isCorrect) {
 			score++;
@@ -180,7 +230,7 @@ public class MultiChoiceTestController {
 	 * Creates an event handler and sets the handleAnswerClick as the handler with
 	 * the provided {@code isCorrect} argument.
 	 */
-	private EventHandler<ActionEvent> getAnswerClickHandler(boolean isCorrect) {
+	private EventHandler<ActionEvent> createAnswerClickHandler(boolean isCorrect) {
 		return new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent e) {
@@ -201,6 +251,12 @@ public class MultiChoiceTestController {
 		alert.showAndWait();
 	}
 
+	void handleNextButtonClick() {
+		resetSelection();
+		incCurrentQuestionIndex();
+		showCurrentQuestion();
+	}
+
 	/**
 	 * Increment {@code currentQ} attribute by one.
 	 */
@@ -208,35 +264,53 @@ public class MultiChoiceTestController {
 		this.currQuestionIndex++;
 	}
 
+	/**
+	 * After user answered all questions in test
+	 */
 	private void handleTestResults() {
-		answersGrid.getChildren().removeAll(answerButtons);
-		answersGrid.getChildren().remove(nextButton);
+		removeAnswerNodes();
+		nextButton.setText("RE-TAKE TEST");
+		nextButton.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				initializeTest();
+			}
+		});
 		questionLabel.setText("You finished the test! Great job");
-		Label scoreTitle = new Label("Your score is");
-		answersGrid.add(scoreTitle, 0, 0);
-		Label score = new Label(Double.toString(calculateScore()));
-		answersGrid.add(score, 0, 1);
+		addCenteredLabelToAnswersGrid("Your score is");
+		double scorePerc = calculateScorePercentage();
+		String scoreToDisplay = Double.toString(formatToTwoDecimals(scorePerc));
+		addCenteredLabelToAnswersGrid(scoreToDisplay);
+		if (scorePerc >= 70) {
+			addCenteredLabelToAnswersGrid("Excellent!");
+		}
+	}
+
+	private void addCenteredLabelToAnswersGrid(String text) {
+		Label l = new Label(text);
+		l.setPrefWidth(answersGrid.getWidth());
+		l.setAlignment(Pos.CENTER);
+		answersGrid.add(l, 0, answersGrid.getChildren().size());
+		resultsLabels.add(l);
 	}
 
 	/**
 	 * Calculates the score and converts it to percentage
+	 * 
 	 * @return the calculated score.
 	 */
-	private double calculateScore() {
-		double scorePerc = ((double) score / questions.size()) * 100;
-		return formatToTwoDecimals(scorePerc);
+	private double calculateScorePercentage() {
+		return ((double) score / questions.size()) * 100;
 	}
 
 	/**
 	 * @return a double formatted to have only 2 digits after dot
 	 */
-    private double formatToTwoDecimals(double value) {
-        // 
-        DecimalFormat decimalFormat = new DecimalFormat("#.##");
-        String formattedValue = decimalFormat.format(value);
+	private double formatToTwoDecimals(double value) {
+		DecimalFormat decimalFormat = new DecimalFormat("#.##");
+		String formattedValue = decimalFormat.format(value);
 
-        return Double.parseDouble(formattedValue);
-    }
-
+		return Double.parseDouble(formattedValue);
+	}
 
 }
